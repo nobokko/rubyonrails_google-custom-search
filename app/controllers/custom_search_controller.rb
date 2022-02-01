3fa742e8662ab408edd831a36edcb09dda7888b3
nobokko <nobokko+github@gmail.com>
2022-02-01 22:18:40 +0900

    最低限の検索画面としての体裁を整える 設定をgoogle_apis.ymlへ記載
    google_apis.yml.sample は　google_apis.yml にリネームする作業あり
    
    ダミーファイルは「Ruby on rails」で検索した結果を保存

--- app/controllers/custom_search_controller.rb
+++ app/controllers/custom_search_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'google/apis/customsearch_v1'
+require 'json'
 
 # custom_search
 class CustomSearchController < ApplicationController
@@ -9,9 +10,31 @@ class CustomSearchController < ApplicationController
   # 一見さんに検索機能は使わせない施策その1 - BASIC認証
   http_basic_authenticate_with name: 'admin', password: 'admin', except: :index
 
+  def initialize
+    super
+    @api_key = Rails.configuration.x.google_apis.customsearch[:api_key]
+    @cse_key = Rails.configuration.x.google_apis.customsearch[:cse_key]
+  end
+
   def index; end
 
   def search
-    render json: { data: 'Hello, World', keywords: params[:keywords], activatorkey: params[:activatorkey] }
+    if 'false'.eql?(params[:usedummy])
+      results = search_from_google_apis(params[:keywords])
+    else
+      file = File.open('searcher_sample.json')
+      results = JSON.parse(file.read)
+    end
+    render json: { keywords: params[:keywords], activatorkey: params[:activatorkey], usedummy: params[:usedummy],
+                   results: results }
+  end
+
+  private
+
+  def search_from_google_apis(query)
+    searcher = Google::Apis::CustomsearchV1::CustomSearchAPIService.new
+    searcher.key = @api_key
+
+    searcher.list_cses(q: query, cx: @cse_key)
   end
 end
--- app/javascript/packs/custom_search.ts
+++ app/javascript/packs/custom_search.ts
@@ -2,21 +2,25 @@ const button_search = document.getElementById('button_search') as HTMLInputEleme
 const loader_container = document.getElementById('loader_container') as HTMLElement;
 const keywords_textbox = document.getElementById('keywords_textbox') as HTMLInputElement;
 const activatorkey_textbox = document.getElementById('activatorkey_textbox') as HTMLInputElement;
+const use_dummy_checkbox = document.getElementById('use_dummy') as HTMLInputElement;
 
 button_search.addEventListener('click', ev => {
     loader_container.style.display = '';
 
     const keywords = keywords_textbox.value;
     const activatorkey = activatorkey_textbox.value;
+    const usedummy = use_dummy_checkbox.checked;
 
     const method = "POST";
     const body = ((params) => {
         return Object.keys(params).map((key) => key + "=" + encodeURIComponent(params[key])).join("&");
-    })({ keywords, activatorkey, });
+    })({ keywords, activatorkey, usedummy, });
     const headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
     };
+    const ul = document.getElementById('search_result');
+    ul.innerHTML = '';
     fetch('/custom_search/search', { method, headers, body, }).then(res => {
         console.log({ res });
         return Promise.all([
@@ -25,12 +29,139 @@ button_search.addEventListener('click', ev => {
         ]);
     }).then(([json]) => {
         console.log({ json });
-        const li = document.createElement('li');
-        li.innerText = JSON.stringify(json);
-        const ul = document.getElementById('search_result');
-        // ul.appendChild(li);
-        ul.prepend(li);
+        let timing = 50;
+        for (const item of json?.results?.items as TypeGoogleApiCustomSearchResultItem[]) {
+            const searchresult_card = document.createElement('searchresult-card') as CustomHTMLCardElement;
+            // searchresult_card.innerText = JSON.stringify(json);
+            searchresult_card.embedParams(item);
+            searchresult_card.removeClassInitialState(timing);
+            const li = document.createElement('li');
+            li.appendChild(searchresult_card);
+            ul.appendChild(li);
+            // ul.prepend(li);
+            timing += 50;
+        }
     }).finally(() => {
         loader_container.style.display = 'none';
     });
 });
+
+type TypeGoogleApiCustomSearchResultItem = ({
+    pagemap?: {
+        [key: string]: ({
+            [key: string]: string
+        }[])
+    }
+} & {
+    [key: string]: string
+})
+
+class CustomHTMLCardElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(this.template());
+    }
+
+    template() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <style>
+        .card{
+            display: flex;
+            flex-direction: column-reverse;
+            width: 288px;
+            max-height: 66vh;
+            min-height: 33vh;
+            border-radius: 8px;
+            overflow: hidden;
+            box-shadow: 0 4px 15px rgba(0,0,0,.2);
+            transform: scale(1);
+            transition: 0.5s;
+        }
+        .card.initial-state{
+            transform: scale(0);
+        }
+        .card__imgframe{
+            width: 100%;
+            height: auto;
+            background: #fff;
+            box-sizing: border-box;
+            display: flex;
+            align-content: center;
+            justify-content: center;
+        }
+        .card__imgframe img{
+            max-width: 100%;
+            max-height: 100%;
+        }
+        .card__textbox{
+            width: 100%;
+            height: auto;
+            padding: 20px 18px;
+            background: #fff;
+            box-sizing: border-box;
+        }
+        .card__textbox > * + *{
+            margin-top: 10px;
+        }
+        .card__titletext{
+            display: inline-block;
+            font-size: 20px;
+            font-weight: bold;
+            line-height: 125%;
+        }
+        .card__overviewtext{
+            font-size: 12px;
+            line-height: 150%;
+        }
+        </style>
+        <a class="card initial-state">
+            <div class="card__textbox">
+                <div class="card__titletext">
+                    タイトルがはいります。
+                </div>
+                <div class="card__overviewtext">
+                    概要がはいります。
+                </div>
+            </div>
+            <div class="card__imgframe">
+                画像がはいります。
+            </div>
+        </a>
+        `;
+
+        return template.content;
+    }
+
+    embedParams(params: TypeGoogleApiCustomSearchResultItem) {
+        const tag_card__root = this.shadowRoot.querySelector('.card')! as HTMLAnchorElement;
+        tag_card__root.href = params.link;
+
+        const tag_card__titletext = this.shadowRoot.querySelector('.card__titletext')!;
+        tag_card__titletext.innerHTML = params.html_title;
+
+        const tag_card__overviewtext = this.shadowRoot.querySelector('.card__overviewtext')!;
+        tag_card__overviewtext.innerHTML = params.html_snippet;
+
+        const tag_card__imgframe = this.shadowRoot.querySelector('.card__imgframe')! as HTMLDivElement;
+        const cse_image_0_src = params?.pagemap.cse_image?.[0]?.src;
+        tag_card__imgframe.innerHTML = '';
+        if (cse_image_0_src) {
+            const img = document.createElement('img') as HTMLImageElement;
+            img.src = cse_image_0_src;
+            tag_card__imgframe.appendChild(img);
+        } else {
+            tag_card__imgframe.innerText = 'No image'
+        }
+    }
+
+    removeClassInitialState(ms: number) {
+        const tag_card__root = this.shadowRoot.querySelector('.card')! as HTMLAnchorElement;
+        setTimeout(() => {
+            tag_card__root.classList.remove('initial-state');
+        }, ms);
+    }
+}
+customElements.define('searchresult-card', CustomHTMLCardElement);
+
