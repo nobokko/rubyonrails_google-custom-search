dc8a897835ffca881c69358264cfa6fcd370b7be
nobokko <nobokko+github@gmail.com>
2022-02-05 00:56:23 +0900

    暗号化／復号化処理

--- app/helpers/custom_search_helper.rb
+++ app/helpers/custom_search_helper.rb
@@ -1,2 +1,57 @@
+# frozen_string_literal: true
+
+require 'openssl'
+require 'base64'
+
+# custom_search
 module CustomSearchHelper
+  # https://docs.ruby-lang.org/ja/latest/class/OpenSSL=3a=3aCipher.html
+  def self.aes256cbc
+    OpenSSL::Cipher.new('AES-256-CBC')
+  end
+
+  def self.aes256cbc_enc(salt, pass)
+    # 暗号化器を作成する
+    enc = aes256cbc
+    enc.encrypt
+    # 鍵とIV(Initialize Vector)を PKCS#5 に従ってパスワードと salt から生成する
+    key_iv = OpenSSL::PKCS5.pbkdf2_hmac_sha1(pass, salt, 2000, enc.key_len + enc.iv_len)
+    # 鍵とIVを設定する
+    key = key_iv[0, enc.key_len]
+    enc.key = key
+    iv = key_iv[enc.key_len, enc.iv_len]
+    enc.iv = iv
+    [enc, key, iv]
+  end
+
+  def self.encrypt(salt, pass, data)
+    (enc, key, iv) = aes256cbc_enc(salt, pass)
+    # 暗号化する
+    # encrypted_data = ''
+    encrypted_data = enc.update(data) + enc.final
+
+    [key, iv, encrypted_data]
+  end
+
+  def self.encrypt_and_base64encode(salt, pass, data)
+    (key, iv, encrypted_data) = encrypt(salt, pass, data)
+    [Base64.encode64(key), Base64.encode64(iv), Base64.encode64(encrypted_data)]
+  end
+
+  def self.decrypt(key, iv, encrypted_value)
+    dec = aes256cbc
+    dec.decrypt
+    # 暗号化した際の鍵とIVを設定する
+    dec.key = key
+    dec.iv = iv
+    # 復号化を行う
+    dec.update(encrypted_value) + dec.final
+  end
+
+  def self.base64decode_and_decrypt(base64_key, base64_iv, base64_encrypted_data)
+    key = Base64.decode64(base64_key)
+    iv = Base64.decode64(base64_iv)
+    encrypted_data = Base64.decode64(base64_encrypted_data)
+    decrypt(key, iv, encrypted_data)
+  end
 end
--- spec/helpers/custom_search_helper_spec.rb
+++ spec/helpers/custom_search_helper_spec.rb
@@ -11,5 +11,27 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe CustomSearchHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when anything' do
+    it 'is valid encrypt values' do
+      # salt = OpenSSL::Random.random_bytes(8)
+      salt = '\x90\xC61e\x14\xB9\xA9\xB0'
+      (base64_key, base64_iv, base64_encrypted_value) = CustomSearchHelper.encrypt_and_base64encode(
+        salt,
+        'pass',
+        'api key'
+      )
+      expect(base64_key.chomp).to eq('6eQF3+5ONTo0Oiy4BOvUa1sIHLqSkD/b+ctESshx6yo=')
+      expect(base64_iv.chomp).to eq('BAUj3vvrUvr6t9eX2FkNPA==')
+      expect(base64_encrypted_value.chomp).to eq('7P1D878nRpzN22R4NswcNQ==')
+    end
+
+    it 'is valid decrypt values' do
+      api_key = CustomSearchHelper.base64decode_and_decrypt(
+        '6eQF3+5ONTo0Oiy4BOvUa1sIHLqSkD/b+ctESshx6yo=',
+        'BAUj3vvrUvr6t9eX2FkNPA==',
+        '7P1D878nRpzN22R4NswcNQ=='
+      )
+      expect(api_key).to eq('api key')
+    end
+  end
 end
