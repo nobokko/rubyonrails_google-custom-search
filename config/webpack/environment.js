680555e45ac541987e050b910ef6b9776c128cba
nobokko <nobokko+github@gmail.com>
2022-02-05 14:28:44 +0900

    htmlを分離

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.prepend('html', {
+    test: /\.html$/,
+    use: [{
+        loader: 'html-loader',
+        options: {
+            minimize: true,
+        }
+    }]
+});
+
 module.exports = environment
